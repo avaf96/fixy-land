b602c0c9bbb5810eede17f6e52b6a05e8ea75c13
omidfattahi <om.fattahi@gmail.com>
2022-02-25 03:54:52 -0800

    signup specialist 3

--- script/signup_specialist.js
+++ script/signup_specialist.js
@@ -17,20 +17,20 @@
 	$(document).ready(function(){
 
 		let dastrasi_sari_grid = $(".dastrasi-sari-grid");
-		let dastrasi_sari_grid_active = $(".dastrasi-sari-grid-active");
 		let swiper_slide = $(".swiper-slide");
-		let squre_div = $(".squre-div");
+		
 
 
 		for(let i = 0 ; i < swiper_slide.length ; i++){
 			$(swiper_slide[i]).click(function(){	
 				for(let j = 0 ; j < swiper_slide.length ; j++){
 					$(dastrasi_sari_grid[j]).addClass("d-none");
-//					$(dastrasi_sari_grid_active[0]).addClass("d-none");
+					
+                    $(swiper_slide[j]).removeClass("darkYellow");
 				
 				}
 				$(dastrasi_sari_grid[i]).removeClass("d-none");	
-  
+                $(swiper_slide[i]).addClass("darkYellow");
 
 			})	
 		}
