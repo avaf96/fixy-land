6810607f91257d09bfd0c2128632acba78916b97
omidfattahi <om.fattahi@gmail.com>
2022-02-26 23:25:29 -0800

    signup - specialist

--- script/signup_specialist.js
+++ script/signup_specialist.js
@@ -12,7 +12,11 @@
       });
 //		swipper-end
 		
-		
+		let form_control_register_specialist_div = $(".form-control-register-specialist-div div div");
+		let form_control_register_specialist_div2 = $(".form-control-register-specialist-div2");
+		let form_control_register_specialist_main = $(".form-control-register-specialist-div");
+        let categories_input = $(".categories-input");
+
 
 	$(document).ready(function(){
 
@@ -34,5 +38,56 @@
 
 			})	
 		}
+		
+		
+		
+		
+		
+		
+		for(let i = 0 ; i < form_control_register_specialist_div.length ; i++){
+			$(form_control_register_specialist_div[i]).click( function(){
+			 for(let j = 0 ; j < form_control_register_specialist_div2.length ; j++){
+				 $(form_control_register_specialist_div2[j]).addClass("d-none");
+			 }
+				$(form_control_register_specialist_div2[i]).removeClass("d-none");
+				$(form_control_register_specialist_main[0]).addClass("d-none");
+		})
+			
+			
+		}
+		
+//		for(let i = 0 ; i < categories_input.length ; i++){
+//			
+//			if($(categories_input[i]).prop('checked', true))
+//			 // Checks it
+//            $('#myCheckbox').prop('checked', false); // Unchecks it
+//			
+//			
+//		}
 
-	})
+		
+		
+		
+
+	});
+
+		   function backToCategories() {
+			   	$(document).ready(function(){
+            for(let j = 0 ; j < form_control_register_specialist_div2.length ; j++){
+				 $(form_control_register_specialist_div2[j]).addClass("d-none");
+				
+			 }
+			$(form_control_register_specialist_main[0]).removeClass("d-none");
+				});
+         }
+	
+   
+
+//$('#myCheckbox').prop('checked', true); // Checks it
+//$('#myCheckbox').prop('checked', false); // Unchecks it
+
+
+
+
+
+
