70746b5455cc17daf50583d2dfae0137071f00b3
Ava Faghihi <a.faghihi75@gmail.com>
2022-03-06 12:43:03 +0330

    update spc-desk

--- script/spc-desk.js
+++ script/spc-desk.js
@@ -36,28 +36,59 @@ $(document).ready(function(){
 	}
 	
 	
-	
+
+//	پیشنهادات
 //	short & long description 
 	
 	//	more info button
-	let more_inf_btn = $("#more_inf_btn");	
+	let more_inf_btn = $(".more_inf_btn");	
 	//	go back button
-	let back_btn = $("#go-back-btn");
+	let back_btn = $(".go-back-btn");
 	
 	let req_short_dsc = $(".req-short-dsc");
 	let req_long_dsc = $(".req-long-dsc");
 	
-	$(more_inf_btn).click(function(){
-		$(req_short_dsc[0]).addClass("d-none");
-		$(req_long_dsc[0]).removeClass("d-none");
+	for(let i=0;i<more_inf_btn.length;i++){
+		$(more_inf_btn[i]).click(function(){
+			$(req_short_dsc[0]).addClass("d-none");
+			$(req_long_dsc[i]).removeClass("d-none");
+		});
+	}
+	
+	
+	for(let i=0;i<back_btn.length;i++){
+		$(back_btn[i]).click(function(){
+			$(req_short_dsc[0]).removeClass("d-none");
+			$(req_long_dsc[i]).addClass("d-none");
+		});
+	}
+	
+	
+	
+//	در دست اجرا
+	//	more info button
+	let job_more_inf_btn = $(".job_more_inf_btn");	
+	let job_short_dsc = $(".job-short-dsc");
+	let job_long_dsc = $(".job-long-dsc");
+	
+	for(let i=0; i<job_more_inf_btn.length; i++){
+		$(job_more_inf_btn[i]).click(function(){
+			$(job_short_dsc[0]).addClass("d-none");
+			$(job_long_dsc[i]).removeClass("d-none");
+		});
+	}
+	
+	
+	
+//	 صفحه اتمام کار توسط متخصص
+	let paying_btn = $("#paying_btn");
+	let running_job = $("#running_job");
+	let spc_req_money = $("#spc_req_money");
+	$(paying_btn).click(function(){
+		$(running_job).addClass("d-none");
+		$(spc_req_money).removeClass("d-none");
 	});
 	
-	$(back_btn).click(function(){
-		$(req_short_dsc[0]).removeClass("d-none");
-		$(req_long_dsc[0]).addClass("d-none");
-	});
-	
-
 	
 	
 })
