f819567747b9aa3d06651671aeb6b98321c9bdd4
Ava Faghihi <a.faghihi75@gmail.com>
2022-03-12 14:54:49 +0330

    update

--- script/spc-income.js
+++ script/spc-income.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 	$("#more-box-btn").click(function(){
 		$("#more_box").toggleClass("d-none");
 		if($("#more_box").hasClass("d-none")){
-			$("#more-box-btn").text("مشاهده بیشتر");
+			$("#more-box-btn").html("مشاهده بیشتر <i class='fa-solid fa-plus'></i>");
 		}else{
-			$("#more-box-btn").text("مشاهده کمتر");
+			$("#more-box-btn").html("مشاهده کمتر <i class='fa-solid fa-minus'></i>");
 		}
 	})
 
--- script/specialistInfo.js
+++ script/specialistInfo.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 	$("#more-cm-btn").click(function(){
 		$("#all-comments").toggleClass("d-none");
 		if($("#all-comments").hasClass("d-none")){
-			$("#more-cm-btn").text("مشاهده بیشتر");
+			$("#more-cm-btn").html("مشاهده بیشتر <i class='fa-solid fa-plus'></i>");
 		}else{
-			$("#more-cm-btn").text("مشاهده کمتر");
+			$("#more-cm-btn").html("مشاهده کمتر <i class='fa-solid fa-minus'></i>");
 		}
 	})
 
